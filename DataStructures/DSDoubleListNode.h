e92af7123a7fa36a34fde595b81c17f6fa1dfaa2
Harshad Kale <harshad.kale@gmail.com>
2014-09-01 12:31:48 -0500

    Added Node inheritance

---
+++ DataStructures/DSDoubleListNode.h
@@ -0,0 +1,13 @@
+//
+//  DSDoubleListNode.h
+//  DataStructures
+//
+//  Created by Harshad Kale on 9/1/14.
+//  Copyright (c) 2014 Harshad Kale. All rights reserved.
+//
+
+#import "DSNode.h"
+
+@interface DSDoubleListNode : DSNode
+
+@end
---
+++ DataStructures/DSTreeNode.h
@@ -0,0 +1,13 @@
+//
+//  DSTreeNode.h
+//  DataStructures
+//
+//  Created by Harshad Kale on 9/1/14.
+//  Copyright (c) 2014 Harshad Kale. All rights reserved.
+//
+
+#import "DSNode.h"
+
+@interface DSTreeNode : DSNode
+
+@end
